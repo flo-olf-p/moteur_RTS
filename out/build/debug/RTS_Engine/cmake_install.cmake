fa389d5b3dc6b41240562e9384ccd969ec818242
Tristan Fabre <tristan.fabre17@gmail.com>
2026-09-09 16:52:05 +0200

    Correction gitignore.

--- out/build/debug/RTS_Engine/cmake_install.cmake
+++ out/build/debug/RTS_Engine/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/trist/source/repos/RTS_Engine/RTS_Engine
+# Install script for directory: C:/Users/trist/Desktop/moteur_RTS/RTS_Engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,6 +35,6 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/trist/source/repos/RTS_Engine/out/build/debug/RTS_Engine/install_local_manifest.txt"
+  file(WRITE "C:/Users/trist/Desktop/moteur_RTS/out/build/debug/RTS_Engine/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- out/build/debug/cmake_install.cmake
+++ out/build/debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/trist/source/repos/RTS_Engine
+# Install script for directory: C:/Users/trist/Desktop/moteur_RTS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,13 +34,13 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/trist/source/repos/RTS_Engine/out/build/debug/RTS_Engine/cmake_install.cmake")
+  include("C:/Users/trist/Desktop/moteur_RTS/out/build/debug/RTS_Engine/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/trist/source/repos/RTS_Engine/out/build/debug/install_local_manifest.txt"
+  file(WRITE "C:/Users/trist/Desktop/moteur_RTS/out/build/debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/trist/source/repos/RTS_Engine/out/build/debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/trist/Desktop/moteur_RTS/out/build/debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
