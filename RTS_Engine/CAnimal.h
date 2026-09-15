a6330a333097ad700934462cfa273dca6b89a0af
Maxence Vechambre <Maxence.VECHAMBRE@etu.uca.fr>
2026-09-15 11:09:00 +0200

    création des classes annimaux

--- RTS_Engine/CAnimal.h
+++ RTS_Engine/CAnimal.h
@@ -11,6 +11,7 @@ private:
     float m_gender;
     float m_time_before_reproduction;
     float m_hunger;
+    std::pair<int,int> m_location;
     
     /*GET------------------------------------------------------*/
 public:
@@ -22,6 +23,7 @@ public:
     float get_gender(){return m_gender;};
     float get_time_before_reproduction(){return m_time_before_reproduction;}
     float get_hunger(){return m_hunger;}
+    std::pair<int,int> get_location(){return m_location;}
     
     /*SET------------------------------------------------------*/
     void set_speed(float speed){m_speed = speed;}
@@ -32,9 +34,11 @@ public:
     void set_gender(float gender){m_gender = gender;}
     void set_time_before_reproduction(float time_before_reproduction){m_time_before_reproduction = time_before_reproduction;}
     void set_hunger(float hunger){m_hunger = hunger;}
+    void set_location(std::pair<int,int> location){m_location = location;}
     
     /*FUNCTION-------------------------------------------------*/
     bool Alive();
+	void story();
 protected:
     void shift();
 };
